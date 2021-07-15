7f4f6dc566cdcf56c1445dae7ce5869313587c4c
topgunaditya13 <87445694+topgunaditya13@users.noreply.github.com>
2021-07-15 09:36:14 -0400

    Update 1sthw.py

--- 1sthw.py
+++ 1sthw.py
@@ -1,18 +1,15 @@
-# at least 8 characters
-# at least one unique character
-
+# Let's start by defining a function; password_validator:#
 def password_validator():
-    unique_characters = ['!', '^', '?', '+', '*']
-    hasUniqueCharacter = False
+    unique_characters=['!', '^', '?', '+', '*'] #This part is giving a list of unique characters that the computer stores; which we can later use to validate a password
+    has_unique_character=False #Setting the value of a variable unique characters to false#
     while True:
-        password = input("Please enter a password: ")
-        "IL!keDogs3"
-# checking if unique characters are in password
-        for char in password:
-            if char in unique_characters:
-                hasUniqueCharacter = True
-        if len(password) < 8 or hasUniqueCharacter is False:
-            password = input("Please enter a password: ")
-        else:
-            break
-    print("Sucess! password is valid. ")
+        password=input("Please enter a password") #Asking for Input#
+        for char in password: #For loop that is scanning each character in input#
+            if char in unique_characters: #Conditional#
+                hasUniqueCharacter=True #Setting this value to true now that there is a unique character#
+                if len(password) >= 8 and hasUniqueCharacter is True: #Nested conditional#
+                    break
+                    print("SUCESS")
+            else: #Anything besides these conditions will make this conditional true.#
+                password = input("Please enter a valid password")
+password_validator()     

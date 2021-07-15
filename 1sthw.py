1fa2e860da0b0b67d16e3dce4dca352ed0e08e64
topgunaditya13 <87445694+topgunaditya13@users.noreply.github.com>
2021-07-15 08:57:23 -0400

    Update 1sthw.py

--- 1sthw.py
+++ 1sthw.py
@@ -1,3 +1,18 @@
+# at least 8 characters
+# at least one unique character
+
 def password_validator():
     unique_characters = ['!', '^', '?', '+', '*']
-    
+    hasUniqueCharacter = False
+    while True:
+        password = input("Please enter a password: ")
+        "IL!keDogs3"
+# checking if unique characters are in password
+        for char in password:
+            if char in unique_characters:
+                hasUniqueCharacter = True
+        if len(password) < 8 or hasUniqueCharacter is False:
+            password = input("Please enter a password: ")
+        else:
+            break
+    print("Sucess! password is valid. ")
